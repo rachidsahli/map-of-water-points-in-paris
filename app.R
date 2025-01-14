ba1b50afdbe366a220355e8809b3f33c154d2f0c
Rachid Sahli <134428938+rachidsahli@users.noreply.github.com>
2025-01-14 15:14:11 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,131 @@
+# Import library --------------- 
+library(shiny) # Application
+library(bslib) # Application
+library(htmltools) # Manipulation de contenu html
+library(dplyr) # Manipulation de données
+library(leaflet) # Carte
+library(leaflet.extras) # Carte
+
+# Import data --------------- 
+# eau <- read.csv("eau.csv")  # Décommentez cette ligne pour charger vos données.
+
+# UI --------------- 
+ui <- page_navbar(
+  theme = bs_theme(bootswatch = "yeti"),
+  title = "Fontaines de Paris",  
+  id = "page",
+  lang = "fr",
+  footer = div(p("© 2025 Fontaines de Paris - Tous droits réservés"), style = "text-align: center;"),
+  bg = "#0A8EDB",
+  
+  layout_sidebar(
+    sidebar = sidebar(
+      title = "Menu",
+      
+      selectInput("Lieu_filtre", "Choisissez un lieu", 
+                  choices = c("Tous", unique(eau$Nom_df)), selected = "Tous"),
+    ),
+    
+    div(
+      style = "padding: 10px; text-align: center;",
+      h4("Où boire de l'eau à Paris ?"),
+      p("Paris regorge de fontaines publiques où vous pouvez vous désaltérer gratuitement. Cette carte interactive vous permet de localiser les fontaines et de découvrir où elles se trouvent dans la ville. Explorez les différents types de fontaines et leur accessibilité pour vous rafraîchir pendant vos promenades.")
+    ),
+    
+    card(
+      full_screen = TRUE,
+      div(
+        style = "position: relative; height: 100%;",  
+        leafletOutput("carte", width = "100%", height = "100%")
+      ),
+      card_footer(
+        popover(
+          a("Learn more", href = "#"),
+          markdown("")
+        )
+      )
+    )
+  ),
+  
+  nav_spacer(),
+  nav_item(
+    a(shiny::icon("github"), "", href = "https://github.com/rachidsahli", target = "_blank", class = "me-3")
+  ),
+  nav_item(
+    a(shiny::icon("linkedin"), "", href = "https://www.linkedin.com/in/rachidsahli/", target = "_blank", class = "me-3")
+  ),
+  nav_item(
+    input_dark_mode()
+  ),
+  nav_item(
+    actionButton("quit_button", "Quitter", icon = icon("sign-out-alt"), class = "btn-danger")
+  )
+)
+
+# SERVER --------------- 
+server <- function(input, output) {
+  
+  # Activation boutons quitter
+  observeEvent(input$quit_button, {
+    stopApp()  
+  })
+  
+  # Filtre lieu
+  eau_filtre <- reactive({
+    if (input$Lieu_filtre == "Tous") {
+      return(eau)
+    } else {
+      return(subset(eau, Nom_df == input$Lieu_filtre))
+    }
+  })
+  
+  # Carte points d'eau
+  output$carte <- renderLeaflet({
+    leaflet(eau_filtre()) %>%
+      addTiles() %>%
+      addAwesomeMarkers(
+        ~lon, ~lat,
+        popup = ~paste0(
+          Lieu,
+          ifelse(!is.na(payant), paste0("<br> Payant : ", payant), ""),
+          ifelse(!is.na(Numéro.voie), paste0("<br> Adresse : ",Numéro.voie," ",Nom.voie), ""),
+          ifelse(!is.na(Voie), paste0("<br>",Voie),""),
+          ifelse(!is.na(adresse.x), paste0("<br> Adresse : ",adresse.x),""),
+          ifelse(!is.na(horaires_lundi), paste0("<br> Lundi : ",horaires_lundi),""),
+          ifelse(!is.na(horaires_mardi), paste0("<br> Mardi : ",horaires_mardi),""),
+          ifelse(!is.na(horaires_mercredi), paste0("<br> Mercredi : ",horaires_mercredi),""),
+          ifelse(!is.na(horaires_jeudi), paste0("<br> Jeudi : ",horaires_jeudi),""),
+          ifelse(!is.na(horaires_vendredi), paste0("<br> Vendredi : ",horaires_vendredi),""),
+          ifelse(!is.na(horaires_samedi), paste0("<br> Samedi : ",horaires_samedi),""),
+          ifelse(!is.na(horaires_dimanche), paste0("<br> Dimanche : ",horaires_dimanche),""),
+          ifelse(!is.na(internet_1), paste0("<br> ",internet_1),"")
+        ),
+        icon = ~awesomeIcons(
+          icon = ~Icons,  
+          iconColor = "black",
+          library = "fa",
+          markerColor = ~Couleur  
+        ),
+        clusterOptions = markerClusterOptions()
+      ) %>%
+      addLegend(
+        position = "topright",
+        colors = c("purple", "lightblue", "orange", "lightgreen"),
+        labels = c("Commerce", "Fontaine", "Bâtiments public", "Espace vert"),
+        title = "Lieu",
+        opacity = 0.9
+      ) %>%
+      addSearchOSM(
+        options = searchOptions(
+          zoom = 15,
+          textPlaceholder = "Rechercher un lieu en France..."
+        )
+      ) %>%
+      addFullscreenControl() %>% 
+      setView(lng = 2.333333, lat = 48.866667, zoom = 11)
+  })
+}
+
+# RUN APP --------------- 
+
+shinyApp(ui = ui, server = server)
